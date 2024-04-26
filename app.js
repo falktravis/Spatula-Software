b817829fdd3c794ef04186866b465bf15ba02fa2
skateeveryday10 <90583375+skateeveryday10@users.noreply.github.com>
2024-04-26 15:56:55 -0400

    Update app.js
    increase start task times

--- app.js
+++ app.js
@@ -664,7 +664,7 @@ const executeCommand = async (interaction) => {
                         }
                         
                         Channel.send("Created " + taskObj.Name);
-                        await new Promise(r => setTimeout(r, Math.floor(Math.random() * 10000 + 10000)));
+                        await new Promise(r => setTimeout(r, Math.floor(Math.random() * 15000 + 20000)));
                     }else{
                         await taskDB.deleteOne({_id: taskObj._id});
                     }
